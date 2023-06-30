e14140e3f905fbe0bd00ee5c102555a777963722
ribeiromauri <110779735+ribeiromauri@users.noreply.github.com>
2023-06-30 11:13:02 -0300

    Agregar archivos de proyecto.

---
+++ TareasMVC/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+
+namespace TareasMVC
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Si necesito cambiar propiedades de las entidades y no quiero usar anotaciones 
+            //modelBuilder.Entity<Tarea>().Property(t => t.Titulo).HasMaxLength(250).IsRequired();
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
