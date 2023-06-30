ad82a8d72c5fec5dc96034360d633c1b1cba0982
ribeiromauri <110779735+ribeiromauri@users.noreply.github.com>
2023-06-30 16:43:05 -0300

    roles de seguridad

--- TareasMVC/Controllers/UsuariosController.cs
+++ TareasMVC/Controllers/UsuariosController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using TareasMVC.Migrations;
 using TareasMVC.Models;
 using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using TareasMVC.Servicios;
 
 namespace TareasMVC.Controllers
 {
@@ -12,11 +14,14 @@ namespace TareasMVC.Controllers
     {
         private readonly UserManager<IdentityUser> userManager;
         private readonly SignInManager<IdentityUser> signInManager;
+        private readonly ApplicationDbContext context;
 
-        public UsuariosController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public UsuariosController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, 
+            ApplicationDbContext applicationDbContext)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+            context = applicationDbContext;
         }
         [AllowAnonymous]
         public IActionResult Registro()
@@ -97,65 +102,54 @@ namespace TareasMVC.Controllers
             var propiedades = signInManager.ConfigureExternalAuthenticationProperties(proveedor, urlRedireccion);
             return new ChallengeResult(proveedor, propiedades);
         }
-        [AllowAnonymous]
-        public async Task<IActionResult> RegistrarUsuarioExterno(string urlRetorno = null, string remoteError = null)
+
+        [HttpGet]
+        [Authorize(Roles = Constantes.RolAdmin)]
+        public async Task<IActionResult> Listado(string mensaje = null)
         {
-            urlRetorno = urlRetorno ?? Url.Content("~/");
-            var mensaje = "";
-
-            if(remoteError is not null)
+            var usuarios = await context.Users.Select(u => new UsuarioViewModel
             {
-                mensaje = $"Error del proveedor externo: {remoteError}";
-                return RedirectToAction("login", routeValues: new { mensaje });
+                Email = u.Email
+            }).ToListAsync();
+
+            var modelo = new UsuariosListadoViewModel();
+            modelo.Usuarios = usuarios;
+
+            modelo.Mensaje = mensaje;
+
+            return View(modelo); 
+        }
+        [HttpPost]
+        [Authorize(Roles = Constantes.RolAdmin)]
+        public async Task<IActionResult> HacerAdmin(string email)
+        {
+            //Busco la primera coincidencia del usuario con el email que recibo
+            var usuario = await context.Users.Where(u => u.Email == email).FirstOrDefaultAsync();
+            if(usuario is null)
+            {
+                return NotFound();
             }
 
-            var info = await signInManager.GetExternalLoginInfoAsync();
-            if(info is null)
+            await userManager.AddToRoleAsync(usuario, Constantes.RolAdmin);
+
+            return RedirectToAction("Listado",
+                routeValues: new { mensaje = "Rol asignado a " + email});
+        }
+        [HttpPost]
+        [Authorize(Roles = Constantes.RolAdmin)]
+        public async Task<IActionResult> RemoverAdmin(string email)
+        {
+            //Busco la primera coincidencia del usuario con el email que recibo
+            var usuario = await context.Users.Where(u => u.Email == email).FirstOrDefaultAsync();
+            if (usuario is null)
             {
-                mensaje = "Error cargado la data del login externo";
-                return RedirectToAction("login", routeValues: new { mensaje }); 
+                return NotFound();
             }
 
-            var resultadoLoginExterno = await signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey,
-                isPersistent: true, bypassTwoFactor: true);
+            await userManager.RemoveFromRoleAsync(usuario, Constantes.RolAdmin);
 
-            //Ya la cuenta existe
-            if (resultadoLoginExterno.Succeeded)
-            {
-                return LocalRedirect(urlRetorno);
-            }
-
-            string email = "";
-            if(info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
-            {
-                email = info.Principal.FindFirstValue(ClaimTypes.Email);
-            }
-            else
-            {
-                mensaje = "Error leyendo el email del usuario del proveedor";
-                return RedirectToAction("login", routeValues: new { mensaje });
-            }
-
-            var usuario = new IdentityUser() { Email = email, UserName = email };
-
-            var resultadoCrearUsuario = await userManager.CreateAsync(usuario);
-            
-            if(!resultadoCrearUsuario.Succeeded)
-            {
-                mensaje = resultadoCrearUsuario.Errors.First().Description;
-                return RedirectToAction("login", routeValues: new { mensaje });
-            }
-
-            var resultadoAgregarLogin = await userManager.AddLoginAsync(usuario, info);
-
-            if(resultadoAgregarLogin.Succeeded)
-            {
-                await signInManager.SignInAsync(usuario, isPersistent: true, info.LoginProvider);
-                return LocalRedirect(urlRetorno);
-            }
-
-            mensaje = "Ha ocurrido un error agregando el login";
-            return RedirectToAction("login", routeValues: new { mensaje });
+            return RedirectToAction("Listado",
+                routeValues: new { mensaje = "Rol removido a " + email });
         }
     }    
 
---
+++ TareasMVC/Migrations/20230630182615_adminRol.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class adminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT EXISTS 
+                                    (SELECT Id FROM AspNetRoles WHERE Id = 'd4c450b5-d463-45d6-8fe6-e48fbc72ae7e')
+                                    BEGIN
+	                                    INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	                                    VALUES ('d4c450b5-d463-45d6-8fe6-e48fbc72ae7e', 'admin', 'ADMIN')
+                                    END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE AspNetRoles WHERE Id = 'd4c450b5-d463-45d6-8fe6-e48fbc72ae7e'");
+        }
+    }
+}
